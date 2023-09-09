2ba9f47e08a2843f7bd4a683a6a55441d998a8cb
LimJunseok <98313823+limjunseok0929@users.noreply.github.com>
2023-09-09 19:47:21 +0900

    20230909

---
+++ README.md
@@ -0,0 +1,7 @@
+# 구글 티렉스 게임 클론코딩
+
+### 플레이 방법
+
+1. 커맨드에 `git clone https://github.com/limjunseok0929/trex-clone.git` 명령어 입력
+
+2. index.html 파일 실행
--- main.js
+++ main.js
@@ -1,6 +1,55 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
+let timer = 0;
+let cactuses = []
+
 canvas.width = window.innerWidth - 100
 canvas.height = window.innerHeight - 100
 
+const ExtendClass = (xCoor, color) => {
+    return class {
+        constructor() {
+            this.x = xCoor
+            this.y = 200
+            this.width = 50
+            this.height = 50
+        }
+        draw() {
+            ctx.fillStyle = color
+            ctx.fillRect(this.x, this.y, this.width, this.height)
+        }
+    }
+}
+
+class Trex extends ExtendClass(10, 'green') {}
+class Cactus extends ExtendClass(500, 'red') {}
+
+const trex = new Trex()
+const cactus = new Cactus()
+
+
+trex.draw()
+cactus.draw()
+
+const Frame = () => {
+    requestAnimationFrame(Frame)
+    timer++;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    if(timer % 120 == 0) {
+        cactuses.push(cactus)
+    }
+
+    cactuses.forEach((a, i, o) => {
+        if(a.x < 0) {
+            o.splice(i, 1)
+        }
+        a.x--
+        a.draw()
+    })
+
+    trex.draw()
+}
+Frame()
