8b7c4d7e8e3271907a61c091d317bf895955d863
LimJunseok <98313823+limjunseok0929@users.noreply.github.com>
2023-09-09 20:08:54 +0900

    20230909

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # 구글 티렉스 게임 클론코딩
-
-### 플레이 방법
-
-1. 커맨드에 `git clone https://github.com/limjunseok0929/trex-clone.git` 명령어 입력
-
-2. index.html 파일 실행
--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext('2d')
 
 let timer = 0;
 let cactuses = []
+let jump = false
+let jumpTimer = 0
 
 canvas.width = window.innerWidth - 100
 canvas.height = window.innerHeight - 100
@@ -26,11 +28,9 @@ class Trex extends ExtendClass(10, 'green') {}
 class Cactus extends ExtendClass(500, 'red') {}
 
 const trex = new Trex()
-const cactus = new Cactus()
 
 
 trex.draw()
-cactus.draw()
 
 const Frame = () => {
     requestAnimationFrame(Frame)
@@ -39,6 +39,7 @@ const Frame = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     if(timer % 120 == 0) {
+        const cactus = new Cactus()
         cactuses.push(cactus)
     }
 
@@ -46,10 +47,29 @@ const Frame = () => {
         if(a.x < 0) {
             o.splice(i, 1)
         }
-        a.x--
+        a.x -= 3
         a.draw()
     })
 
+    if(jump) {
+        trex.y -= 3
+        jumpTimer++
+    }
+    if(!jump) {
+        if(trex.y < 200){
+            trex.y += 3
+        }
+    }
+    if(jumpTimer == 50){
+        jump = false
+        jumpTimer = 0
+    }
     trex.draw()
 }
 Frame()
+
+document.addEventListener('keydown', e => {
+    if(e.code == 'Space') {
+        jump = true
+    }
+})
